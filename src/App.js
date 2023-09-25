0a53459cd1c4a205b735697c728948e0a8d345f2
kirusolo <kirubelendala15@gmail.com>
2023-09-26 00:15:29 +0200

    working my contact page using emailjs and also add my realtime location using react leafler

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
-import Loader from 'react-loaders'
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
           <Route path="/Contact" element={<Contact />}></Route>
         </Route>
       </Routes>
-      <Loader />
     </>
   )
 }
